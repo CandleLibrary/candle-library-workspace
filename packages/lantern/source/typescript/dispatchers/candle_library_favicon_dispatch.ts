758f1f9c6fd93cbccad32d9ef8680df96df6b348
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-12-04 13:17:56 -0700

    Improve favicon identification and serving

--- packages/lantern/source/typescript/dispatchers/candle_library_favicon_dispatch.ts
+++ packages/lantern/source/typescript/dispatchers/candle_library_favicon_dispatch.ts
@@ -1,18 +1,45 @@
 
-import { Dispatcher } from "../types/types";
-import ext_map, { addKey } from "../extension/extension_map.js";
-import URL from "@candlelib/uri";
+import URI from "@candlelib/uri";
 import path from "path";
-import fs from "fs";
+import ext_map, { addKey } from "../extension/extension_map.js";
+import { Dispatcher } from "../types/types";
 
 addKey("ico", ext_map);
+addKey("png", ext_map);
+addKey("jpg", ext_map);
 
-const favicon_path = path.join((new URL(import.meta.url)).path, "../../../brand/candle.ico");
+const
+    default_favicon = path.join((new URI(import.meta.url)).path, "../../../brand/candle.ico");
 
+
+let favicon_path = default_favicon;
 export default <Dispatcher>{
     name: "CandleLib Default favicon",
     description: `CandleLib Default favicon`,
     MIME: "image/vnd.microsoft.icon",
-    respond: fs.readFileSync(favicon_path),
-    keys: [{ ext: ext_map.ico, dir: "/*" }]
+    init: async (lantern, self) => {
+
+        const CWD = <URI>URI.resolveRelative(<string>self.cwd);
+        const def = { path: <URI>URI.resolveRelative("../../brand/candle.ico", URI.getEXEURL(import.meta)), mime: "image/vnd.microsoft.icon" };
+        const jpg = { path: <URI>URI.resolveRelative("./favicon.jpg", CWD), mime: "image/jpg" };
+        const ico = { path: <URI>URI.resolveRelative("./favicon.ico", CWD), mime: "image/vnd.microsoft.icon" };
+        const png = { path: <URI>URI.resolveRelative("./favicon.png", CWD), mime: "image/png" };
+
+        for (const { path, mime } of [jpg, ico, png, def]) {
+
+            if (await path.DOES_THIS_EXIST()) {
+                self.MIME = mime;
+                favicon_path = path + "";
+                break;
+            }
+        }
+    },
+    respond: (tools) => {
+        if (tools.filename == "favicon") {
+            tools.setMIME();
+            return tools.sendRawStreamFromFile(favicon_path);
+        }
+        return false;
+    },
+    keys: [{ ext: ext_map.ico | ext_map.png | ext_map.jpg, dir: "/*" }]
 };
