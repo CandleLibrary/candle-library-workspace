18d745c0b703825902ac9249679b3d7d8581dd61
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-12-04 13:24:35 -0700

    Remove unused file

--- packages/wick/source/typescript/client/runtime/component/common.ts
+++
@@ -1,9 +0,0 @@
-import { WickRTComponent } from "./component.js";
-
-export function takeParentAddChild(parent: WickRTComponent, child: WickRTComponent) {
-    //@ts-ignore
-    parent.ch.push(child);
-
-    //@ts-ignore
-    child.par = parent;
-};
