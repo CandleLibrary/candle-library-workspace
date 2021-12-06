13dbf37fd5ba1e1041001fc431fb3f1129290b93
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-12-05 19:50:42 -0700

    Refactor: Remove unused components and re-organize component directory

--- packages/wick/source/typescript/workspace/client/types/history_state.ts
+++
@@ -1,5 +0,0 @@
-import { HistoryArtifact } from "./history_artifact";
-
-export interface HistoryState {
-    actions: HistoryArtifact[];
-}
--- packages/wick/source/typescript/workspace/logger.ts
+++
@@ -1,5 +0,0 @@
-import { Logger } from "@candlelib/log";
-
-export const logger = Logger.get("wick");
-Logger.get("lantern");
-Logger.get("wick");
